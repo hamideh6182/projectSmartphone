db6415f12eb8104639109f4e7f4d23de3755a6c9
hamideh <hamideh_h61@yahoo.com>
2023-01-31 14:57:38 +0100

    Solution von Hamideh with Test

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -1,2 +1,21 @@
-public class Contact {
+public abstract class Contact {
+    private String name;
+
+    public Contact(String name) {
+        this.name = name;
+    }
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
